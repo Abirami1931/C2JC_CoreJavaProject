d4cf0d3175176997b8b4547425fa97a6370048e3
Abirami1931 <147375297+Abirami1931@users.noreply.github.com>
2024-08-27 18:58:01 +0530

    day4

--- Executor.java
+++ Executor.java
@@ -1,28 +1,20 @@
-package com.tns.dayfour.firstpackage;
+package com.tns.dayfour.secondpackage;
+
+import com.tns.dayfour.firstpackage.Base;
 
 public class Executor {
 	public static void main(String[] args) {
-		//accessing same package class
-		Base b1=new Base();
-		b1.methodDefault();
-		b1.methodProtected();
-		b1.methodPublic();
-		b1.varDefault=11;
-		b1.methodDefault();
-		//private member can't accessible
-		/*b1.varPrivate=21; 
-		b1.methodPrivate();*/
-		b1.varProtected=31;
-		b1.methodProtected();
-		b1.varPublic=41;
-		b1.methodPublic();
-		//instance of operator
-		System.out.println(b1 instanceof Base); 
-		//returns True
+		// accessing different package class
+		Base b1 = new Base();
+		// private, default and protected members can't access
 		/*
-		* Person p=new Person(); System.out.println(p instanceof Base); //CTE
-		* Incompatible types
-		*/ 
+		* b1.varPrivate=21; b1.methodPrivate(); 
+		b1.varDefault=11; b1.methodDefault();
+		* b1.varProtected=31; b1.methodProtected();
+		*/
+		b1.methodPublic();
+		b1.varPublic = 41;
+		b1.methodPublic();
 		}
 
 
